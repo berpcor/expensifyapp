617167879fb02d02a70d144d5d41590fe081269b
stanislav <mail@mail.ru>
2018-05-28 03:29:36 +0700

    Setup production webpack build тест

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-export default ExpenseList = (props) => {
-    return (
-        <div>
-            <h1>Expense List</h1>
-            
-            {
-                props.expenses.map((expense) => <ExpenseListItem key={expense.id} {...expense}/>)
-            }
+export const ExpenseList = (props) => (
+    <div>
+        {
+            props.expenses.length === 0 ? (
+                <p>No expenses</p>
+            ) : (
+                    props.expenses.map((expense) => {
+                        return <ExpenseListItem key={expense.id} {...expense} />;
+                    })
+                )
+        }
+    </div>
+);
 
-
-        </div>
-    );
-};
-
-// Привязка к Storeв части, которая необходима для данного компонента.
 const mapStateToProps = (state) => {
     return {
         expenses: selectExpenses(state.expenses, state.filters)
-    }
+    };
 };
 
 export default connect(mapStateToProps)(ExpenseList);
-
